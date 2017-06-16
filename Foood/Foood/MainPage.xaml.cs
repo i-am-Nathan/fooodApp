2d2af47459e5c74dc59692f8af28d346922fe192
Nathan situ <nathansitu@hotmail.com>
2017-06-17 01:11:09 +1200

    Initialize base project

---
+++ Foood/Foood/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Plugin.Media;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Foood
+{
+	public partial class MainPage : ContentPage
+	{
+ 
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+	}
+}

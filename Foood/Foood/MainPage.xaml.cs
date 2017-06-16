2205578fbaae62297e78ccc73d33ef77137591c1
Nathan situ <nathansitu@hotmail.com>
2017-06-17 01:13:33 +1200

    Add media plugin for using camera

--- Foood/Foood/MainPage.xaml.cs
+++ Foood/Foood/MainPage.xaml.cs
@@ -17,5 +17,22 @@ namespace Foood
 			InitializeComponent();
 		}
 
+        private async void takeButtonClicked(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            //Make sure that there is a camera and can take photos available
+            if(CrossMedia.Current.IsCameraAvailable && CrossMedia.Current.IsTakePhotoSupported)
+            {
+                var MediaOptions = new Plugin.Media.Abstractions.StoreCameraMediaOptions
+                {
+                    Directory = "Receipts",
+                    Name = $"{DateTime.UtcNow}.jpg"
+                };
+
+                //Takes photo of the business receipt
+                var file = await CrossMedia.Current.TakePhotoAsync(MediaOptions);
+            }
+        }
 	}
 }

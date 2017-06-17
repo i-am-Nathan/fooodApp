72ae8bc63fd02fb7bd353bd7ecc1d2e8fc56a7c1
i-am-Nathan <nathansitu@hotmail.com>
2017-06-17 18:44:04 +1200

    Added Custon vision api

--- Foood/Foood/MainPage.xaml.cs
+++ Foood/Foood/MainPage.xaml.cs
@@ -6,6 +6,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Plugin.Media.Abstractions;
+using System.Diagnostics.Contracts;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Foood.Model;
+using Newtonsoft.Json;
 
 namespace Foood
 {
@@ -17,8 +23,7 @@ namespace Foood
 			InitializeComponent();
 		}
 
-        private async void uploadButtonClicked(object sender, EventArgs e)
-        {
+        private async void uploadButtonClicked(object sender, EventArgs e) { 
 
         }
 
@@ -42,7 +47,61 @@ namespace Foood
                 {
                     return file.GetStream();
                 });
+
+                foodLabel.Text = "";
+
+                await MakePredictionRequest(file);
             }
         }
-	}
+
+        private async Task MakePredictionRequest(MediaFile file)
+        {
+            Contract.Ensures(Contract.Result<Task>() != null);
+
+            var client = new HttpClient();
+
+            client.DefaultRequestHeaders.Add("Prediction-Key", "9b2cd115e674489c892a12aa9b4747f2");
+
+            String url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v1.0/Prediction/f7b0efdf-bdfe-43bd-956a-1fc151887c04/url?iterationId=475ef7ec-5997-401d-95ad-8d6b535614aa";
+
+            HttpResponseMessage response;
+
+            byte[] byteData = GetImageAsByteArray(file);
+
+            using (var content = new ByteArrayContent(byteData))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                response = await client.PostAsync(url, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseString = await response.Content.ReadAsStringAsync();
+                    PredictionModel responseModel = JsonConvert.DeserializeObject<PredictionModel>(responseString);
+
+                    List<Prediction> predictions = responseModel.Predictions;
+
+                    foreach (Prediction prediction in predictions)
+                    {
+                        if (prediction.Probability >= 0.8)
+                        {
+                            foodLabel.Text = prediction.Tag;
+                            file.Dispose();
+                            return;
+                        }
+                    }
+
+                    foodLabel.Text = "What is this? Is this even food?";
+                }
+            }
+        
+            file.Dispose();
+        }
+
+        private byte[] GetImageAsByteArray(MediaFile file)
+        {
+            var stream = file.GetStream();
+            BinaryReader br = new BinaryReader(stream);
+            return br.ReadBytes((int)stream.Length);
+        }
+    }
 }

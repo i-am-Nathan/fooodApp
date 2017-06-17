1725eb86926ff3aa0d39db4ea8476d39d52af00d
i-am-Nathan <nathansitu@hotmail.com>
2017-06-18 00:07:19 +1200

    Updating repo

---
+++ Foood/Foood.Android/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("Foood.Droid.MainApplication, Foood.Android, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md510600391f1546f6003db13113ee56d9d.MainApplication.class, md510600391f1546f6003db13113ee56d9d.MainApplication.__md_methods);
+		
+	}
+}
